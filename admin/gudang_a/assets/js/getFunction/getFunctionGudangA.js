2ec01468a0d0dd94a3807c6a3833b0510b2c2276
Novi <alnoviantiramadhani91@gmail.com>
2024-02-03 20:52:51 +0700

    update js

--- admin/gudang_a/assets/js/getFunction/getFunctionGudangA.js
+++ admin/gudang_a/assets/js/getFunction/getFunctionGudangA.js
@@ -46,8 +46,8 @@ export const tableGudangA = `
 </td>
 <td class="p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent text-center">
   <a type="button" href="edit_gudang_a.html?gudangaId=#EDIT#" class="text-xs font-semibold leading-tight text-slate-400"> <i class="fas fa-edit"></i></a>
-  <button onclick="deleteGudangA('#DELETE#')" class="text-xs font-semibold leading-tight text-slate-400"><a type="button" href="detail_gudang.html?name=#DETAIL#" class="text-xs font-semibold leading-tight text-slate-400"> <i class="fas fa-info-circle"></i></a>
-  <i class="fas fa-trash-alt"></i>
+  <button onclick="deleteGudangA('#DELETE#')" class="text-xs font-semibold leading-tight text-slate-400"><i class="fas fa-trash-alt"></i>
+  <a type="button" href="detail_gudang.html?name=#DETAIL#" class="text-xs font-semibold leading-tight text-slate-400"> <i class="fas fa-info-circle"></i></a>
 </button>
 </td>
 </tr>
